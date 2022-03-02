eccf5a4064ad7ba219542e5b5efe79784cd27191
Cameron Bowers <cameronbowers0@gmail.com>
2022-03-02 14:29:38 -0800

    Added select brewery by ID API

--- routes/brewery/index.js
+++ routes/brewery/index.js
@@ -19,4 +19,27 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    /** Select category by id */
+
+    //Stores the input id from the request to a variable
+    const breweryID = req.params.id  
+
+    // Define our queries
+    let query1 = "SELECT Brewery.name AS 'Brewery', Brewery.streetNumber AS 'Street_Number', " 
+    query1 += "Brewery.streetName AS 'Street_Name', Brewery.city AS 'City', "
+    query1 += "Brewery.zipCode AS 'Zip_Code', Brewery.state AS 'State', "
+    query1 += "Brewery.website AS 'Website', Brewery.breweryID AS 'breweryID' "
+    query1 += "FROM Brewery "
+    query1 += "LIMIT 6 "
+    query1 += "WHERE Brewery.breweryID = ? ;"
+    
+    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    db.pool.query(query1, [breweryID], (err, rows, fields) => {
+        // Send the results to the browser
+        res.setHeader('Content-Type', 'application/json');
+        res.json(rows);
+    });
+});
+
 module.exports = router;
