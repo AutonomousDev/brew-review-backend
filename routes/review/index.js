8fef669974d7840d4fe49529e538c524a6a6f383
Richard Silva <silvari@oregonstate.edu>
2022-03-02 01:08:34 -0700

    Fixed review query

--- routes/review/index.js
+++ routes/review/index.js
@@ -14,20 +14,20 @@ router.get('/:id', (req, res) => {
 
     let query2 = "SELECT Tag.name AS 'Tags' "
     query2 += "FROM Tag "
-    query2 += "INNER JOIN Tag_Review ON Tag.tagID = Tag_Review.tagID "
-    query2 += "INNER JOIN Review ON Tag_Review.reviewID = Review.reviewID "
+    query2 += "INNER JOIN Review_Tag ON Tag.tagID = Review_Tag.tagID "
+    query2 += "INNER JOIN Review ON Review_Tag.reviewID = Review.reviewID "
     query2 += "WHERE Review.reviewID = ?;"
 
     // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
     // This is where review_id is inserted into the query at ?
-    db.pool.query(query1, [review_id], (err, rows, fields) => {
+    db.pool.query(query1, [review_id], (err, rows1, fields) => {
         // Send the results to the browser
-        res.json({rows});
-    });
 
-    db.pool.query(query2, [review_id], (err, rows, fields) => {
-        // Send the results to the browser
-        res.json({rows});
+        db.pool.query(query2, [review_id], (err, rows2, fields) => {
+            // Send the results to the browser
+            rows2 = Object.assign(rows2, rows1);
+            res.json({rows2});
+        });
     });
 });
 
