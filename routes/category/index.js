b54febe2654f7351b30d66f45d0959c9ffc64af0
Cameron Bowers <cameronbowers0@gmail.com>
2022-03-02 10:49:09 -0800

    Added Select category by id

--- routes/category/index.js
+++ routes/category/index.js
@@ -17,4 +17,21 @@ router.get('/category', (req, res) => {
     });
 });
 
+router.get('/category/:id', (req, res) => {
+    /** Select category by id */
+    // Define our queries
+    let query1 = "SELECT Parent_Category.name AS 'Type', Category.name AS 'Name' " 
+    query1 += "FROM Category "
+    query1 += "INNER JOIN Parent_Category "
+    query1 += "ON Category.parentCategoryID = Parent_Category.parentCategoryID "
+    query1 += "WHERE Category.categoryID = ? ";
+
+    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    db.pool.query(query1, (err, rows, fields) => {
+        // Send the results to the browser
+        res.setHeader('Content-Type', 'application/json');
+        res.json(rows);
+    });
+});
+
 module.exports = router;  
