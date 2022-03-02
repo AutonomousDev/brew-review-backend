130dc440257d9a9effb055c2458b77ae1cf3340b
Cameron Bowers <cameronbowers0@gmail.com>
2022-03-02 10:57:10 -0800

    Defined a missing variable

--- routes/category/index.js
+++ routes/category/index.js
@@ -19,6 +19,10 @@ router.get('/category', (req, res) => {
 
 router.get('/category/:id', (req, res) => {
     /** Select category by id */
+
+    //Stores the input id from the request to a variable
+    const categoryID = req.params.id  
+
     // Define our queries
     let query1 = "SELECT Parent_Category.name AS 'Type', Category.name AS 'Name' " 
     query1 += "FROM Category "
@@ -27,7 +31,7 @@ router.get('/category/:id', (req, res) => {
     query1 += "WHERE Category.categoryID = ? ;"
 
     // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-    db.pool.query(query1, (err, rows, fields) => {
+    db.pool.query(query1, [categoryID], (err, rows, fields) => {
         // Send the results to the browser
         res.setHeader('Content-Type', 'application/json');
         res.json(rows);
