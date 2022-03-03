d171c6d88456756270bd2b8c56099d3db5eb4c83
Cameron Bowers <cameronbowers0@gmail.com>
2022-03-02 18:17:05 -0800

    Decomposed review querry into 3 seperate parts.

--- routes/review/index.js
+++ routes/review/index.js
@@ -5,13 +5,29 @@ router.get('/:id', (req, res) => {
     /** Select reviews for a certain beverage */
     console.log("Fetching reviews: " + req.params.id)
     //Stores the input id from the request to a variable
-    const review_id = req.params.id   
+    const review_id = req.params.id
 
     // Define our queries
-    let query1 = "SELECT Review.rating AS 'Rating', Review.textReview AS 'Review_Text' " 
+    let query1 = "SELECT Review.rating AS 'Rating', Review.textReview AS 'Review_Text' "
     query1 += "FROM Review "
     query1 += "WHERE Review.reviewID = ?;"
 
+    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+    // This is where review_id is inserted into the query at ?
+    db.pool.query(query1, [review_id], (err, rows1, fields) => {
+        // Send the results to the browser
+        rows1 = Object.assign(rows1);
+        res.json(rows1);
+    });
+});
+
+router.get('/review-tags/:id', (req, res) => {
+    /** Select tags for a certain review */
+    console.log("Fetching tags for reviews: " + req.params.id)
+    //Stores the input id from the request to a variable
+    const reviewID = req.params.id
+
+    // Define our queries
     let query2 = "SELECT Tag.name AS 'Tags', Tag.tagID AS 'tagID' "
     query2 += "FROM Tag "
     query2 += "INNER JOIN Review_Tag ON Tag.tagID = Review_Tag.tagID "
@@ -20,21 +36,18 @@ router.get('/:id', (req, res) => {
 
     // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
     // This is where review_id is inserted into the query at ?
-    db.pool.query(query1, [review_id], (err, rows1, fields) => {
-        // Send the results to the browser
 
-        db.pool.query(query2, [review_id], (err, rows2, fields) => {
-            // Send the results to the browser
-            rows2 = Object.assign(rows2, rows1);
-            res.json(rows2);
-        });
+    db.pool.query(query2, [reviewID], (err, rows2, fields) => {
+        // Send the results to the browser
+        rows2 = Object.assign(rows2);
+        res.json(rows2);
     });
 });
 
-router.get('beverage/:id', (req, res) => {
+router.get('/beverage/:id', (req, res) => {
     /** Select reviews for beverage */
     console.log("Fetching beverage reviews: " + req.params.id)
-    const beverage_id = req.params.id;
+    const beverageID = req.params.id;
 
     // Define our queries
     let query1 = "SELECT Review.reviewID as 'reviewID' "
@@ -46,7 +59,5 @@ router.get('beverage/:id', (req, res) => {
         res.json(rows);
     });
 });
-    
-
 
 module.exports = router;
